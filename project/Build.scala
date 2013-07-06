d8116e6b33a2dbfe369d493d0eff02f0a5e261be
Dominic Bou-Samra <dominic.bou.samra@gmail.com>
2013-07-07 08:55:56 +1000

    Added Bower for JS dependency management. Need to run 'bower install'

--- project/Build.scala
+++ project/Build.scala
@@ -1,8 +1,6 @@
 import sbt._
 import Keys._
 import play.Project._
-import sbtjslint.Plugin._
-import sbtjslint.Plugin.LintKeys._
 
 
 object ApplicationBuild extends Build {
@@ -15,13 +13,20 @@ object ApplicationBuild extends Build {
     "com.typesafe.play" %% "play-slick" % "0.3.2",
     "com.fasterxml.jackson.module" %% "jackson-module-scala" % "2.1.3"
   )
-  
-  val localSettings = lintSettings ++ inConfig(Compile)(Seq(
-    sourceDirectory in jslint <<= (baseDirectory)(_ / "app/assets/javascripts"),
-    excludeFilter in jslint := "generated" || "lib",
-    flags in jslint := Seq("sloppy", "continue", "vars", "nomen")
-))
 
-  val main = play.Project(appName, appVersion, appDependencies)
-    .settings(localSettings : _*)
+  val main = play.Project(appName, appVersion, appDependencies).settings(
+    // Allow all the specified commands below to be run within sbt
+    commands <++= baseDirectory { base =>
+      Seq(
+        "bower"
+      ).map(cmd(_, base))
+    }
+  )
+
+  private def cmd(name: String, base: File): Command = {
+    Command.args(name, "<" + name + "-command>") { (state, args) =>
+      Process(name :: args.toList, base) !;
+      state
+    }
+  }
 }
